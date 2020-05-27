8d054677ce8594953b1588487cf81badd052ef7b
LeeDaeil <dleodlf1004@chosun.kr>
2020-05-27 10:53:24 +0900

    Day_2 업데이트함.

---
+++ Day_2.py
@@ -0,0 +1,30 @@
+print("안녕하세요.")
+
+test_a = 4
+if test_a == 3:
+    print("3")
+else:
+    print("3이 아님.")
+
+for i in range(0, 10):
+    print(i)
+
+temp = 0
+while True:
+    temp = temp + 1
+    print(temp)
+    if temp > 40:
+        break
+
+temp = 0
+while temp < 40:
+    temp += 1
+    print(temp)
+
+# y = f(x)
+
+def add_function(x):
+    y = x+3
+    return y
+
+print(add_function(3))
